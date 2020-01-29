0a5f7659eb3aa7a7f44cddef607141dcdef639f8
Rupinder <rupinder.developer@gmail.com>
2020-01-29 11:47:58 +0530

    Impletement method chaining in database class

--- app/core/database.php
+++ app/core/database.php
@@ -2,230 +2,86 @@
 /**
  * Database.
  * @author    Rupinder Singh <rupinder.developer@gmail.com>
- * @copyright 2019
+ * @copyright 2020
  */
 namespace MySQL;
 
 require_once dirname(__FILE__).'/config.php';
 
-class Database
-{
+class Database {
     /**
      * Private Variables
      *
      * @var Connection $handler
+     * @var array      $bindParams
+     * @var string     $select
+     * @var array      $where
+     * @var string     $cols
      */
+    private $bindParams;
     private $handler;
+    private $select;
+    private $where;
+    private $cols;
 
-    /**
-     * This functions help to build the connection with Database.
-     *
-     * @return mixed
-     */
-    public function connect()
-    {
+    function __construct() {
+        // Initialization
+        $this->bindParams = [];
+        $this->where = [];
+        $this->cols = '*';
+    }//end __construct()
+
+    public function __destruct() {
+        $this->handler = null;
+    }//end __destruct()
+
+    public function connect() {
         try {
+            // PDO Connection
             $this->handler = new \PDO('mysql:host='.HOSTNAME.';dbname='.DB_NAME, USERNAME, PASSWORD);
             $this->handler->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-            return true;
-        } catch (PDOException $e) {
+            return $this->handler;
+        } catch (\PDOException $e) {
             return $e->getMessage();
         }
     }//end connect()
 
-    /**
-     * Clean up resources here __destruct()
-     *
-     * @return null
-     */
-    public function __destruct()
-    {
-        $this->handler = null;
-    }//end __destruct()
-
-    /**
-     * select() Function.
-     *
-     * @param string $table
-     * @param array  $condition
-     * @param string $glue
-     *
-     * @return array
-     */
-    public function select($select, $condition = [], $glue = 'AND')
-    {
-        if (is_array($select)) {
-            $sql = 'SELECT '.$select[0].' FROM '.$select[1];
-        } else {
-            $sql = 'SELECT * FROM '.$select;
-        }
-
-        if (empty($condition)) {
-            $query = $this->handler->prepare($sql);
-        } else {
-            foreach ($condition as $key => $value) {
-                $pieces[] = $this->binary.' '.$key.'=:'.$key;
-            }
-            $where = ' WHERE '.implode(' '.trim($glue).' ', $pieces);
-            $query = $this->handler->prepare($sql.$where);
-            foreach ($condition as $key => &$value) {
-                $query->bindParam(':'.$key, $value);
-            }
-        }
-
-        $query->execute();
-        $result = $query->fetchAll(\PDO::FETCH_ASSOC);
-        return $result;
+    public function select($tableName) {
+        $this->select = $tableName;
+        return $this;
     }//end select()
 
-    /**
-     * join() Function.
-     *
-     * @param mixed  $select
-     * @param mixed  $join
-     * @param string $on
-     * @param array  $condition
-     * @param string $glue
-     *
-     * @return array
-     */
-    public function join($select, $join, $on, $condition = [], $glue = 'AND')
-    {
-        if (is_array($select)) {
-            $select = 'SELECT '.$select[0].' FROM '.$select[1];
-        } else {
-            $select = 'SELECT * FROM '.$select;
-        }
+    public function cols($cols) {
+        $this->cols = $cols;
+        return $this;
+    }//end cols()
 
-        if (is_array($join)) {
-            $join = ' '.$join[1].' JOIN '.$join[0].' ON '.$on;
-        } else {
-            $join = ' JOIN '.$join.' ON '.$on;
+    public function where($conditions, $glue = 'AND') {
+        $temp = [];
+        foreach($conditions as $key => $value) {
+            array_push($temp, "{$key}=:where_{$key}");
+            $this->bindParams[":where_{$key}"] = $value;
         }
+        array_push($this->where, ' ('.implode(" {$glue} ", $temp).')');
+        return $this;
+    }//end where()
 
-        if (empty($condition)) {
-            $where = '';
-        } else {
-            foreach ($condition as $key => $value) {
-                $pieces[] = $this->binary.' '.$key.'=:'.str_replace('.', '_', $key);
-            }
-            $where = ' WHERE '.implode(' '.trim($glue).' ', $pieces);
-        }
-        $query = $this->handler->prepare($select.$join.$where);
-        foreach ($condition as $key => &$value) {
-            $key = str_replace('.', '_', $key);
-            $query->bindParam(':'.$key, $value);
-        }
-
-        $query->execute();
-        $result = $query->fetchAll(\PDO::FETCH_ASSOC);
-        return $result;
-    }//end join()
-
-    /**
-     * insert() Function.
-     *
-     * @param string $table
-     * @param array  $values
-     *
-     * @return boolean
-     */
-    public function insert($table, $values)
-    {
-        foreach ($values as $key => $value) {
-            $col[] = $key;
-            $val[] = ':'.$key;
-        }
-        $cols  = implode(',', $col);
-        $vals  = implode(', ', $val);
-        $sql   = 'INSERT INTO '.$table.'('.$cols.') VALUES('.$vals.')';
-        $query = $this->handler->prepare($sql);
-        foreach ($values as $key => &$value) {
-            $query->bindParam(':'.$key, $value);
-        }
-        $result = $query->execute();
-        return $result;
-    }//end insert()
-
-    /**
-     * update() Function.
-     *
-     * @param string $table
-     * @param array  $values
-     * @param array  $condition
-     * @param string $glue
-     *
-     * @return boolean
-     */
-    public function update($table, $values, $condition = [], $glue = 'AND')
-    {
-        foreach ($values as $key => $value) {
-            $pieces[] = $key.'=:'.$key;
-        }
-        if (empty($condition) !== true) {
-            foreach ($condition as $key => $value) {
-                $wherePieces[] = $this->binary.' '.$key.'=:where'.$key;
-            }
-            $where = ' WHERE '.implode(' '.trim($glue).' ', $wherePieces);
+    public function execute() {
+        if (count($this->where) > 0) {
+            $where = ' WHERE '.implode(' AND ',$this->where);
         } else {
             $where = '';
         }
-        $update = implode(', ', $pieces);
-        $sql    = 'UPDATE '.$table.' SET '.$update.$where;
-        $query  = $this->handler->prepare($sql);
-        foreach ($values as $key => &$value) {
-            $query->bindParam(':'.$key, $value);
-        }
-        if (empty($condition) !== true) {
-            foreach ($condition as $key => &$value) {
-                $query->bindParam(':where'.$key, $value);
-            }
-        }
-        $result = $query->execute();
-        return $result;
-    }//end update()
+        echo 'SELECT '.$this->cols.' FROM '.$this->select.$where;
 
-    /**
-     * delete() Function.
-     *
-     * @param string $table
-     * @param array  $condition
-     * @param string $glue
-     *
-     * @return boolean
-     */
-    public function delete($table, $condition, $glue = 'AND')
-    {
-        foreach ($condition as $key => $value) {
-            $pieces[] = $this->binary.' '.$key.'=:'.$key;
-        }
-        $where = ' WHERE '.implode(' '.trim($glue).' ', $pieces);
-        $sql   = 'DELETE FROM '.$table.$where;
-        $query = $this->handler->prepare($sql);
-        foreach ($condition as $key => &$value) {
-            $query->bindParam(':'.$key, $value);
-        }
-        $result = $query->execute();
-        return $result;
-    }//end delete()
+        // Cleaning up resources
+        $this->bindParams = [];
+        $this->where = [];
+        $this->cols = '*';
+    }//end execute()
 
-    /**
-     * query() Function.
-     *
-     * @param string $sql
-     * @param array  $bind
-     *
-     * @return Preapared Statement
-     */
-    public function query($sql, $bind = [])
-    {
-        $result = $this->handler->prepare($sql);
-        if (empty($bind) !== true) {
-            foreach ($bind as $key => &$value) {
-                $result->bindParam($key, $value);
-            }
-        }
-        return $result;
+    public function query($sql) {
+        return $this->handler->prepare($sql);
     }//end query()
 
     /**
@@ -239,8 +95,7 @@ class Database
      * @param  $url
      * @return boolean
      */
-    public function installSQL($url)
-    {
+    public function installSQL($url) {
         if ($this->connect() === true) {
             $stmt = file_get_contents($url);
             $query = $this->handler->prepare($stmt);
@@ -259,8 +114,7 @@ class Database
      *
      * @return void
      */
-    public function dropTables()
-    {
+    public function dropTables() {
         if ($this->connect() === true) {
             $sql = 'SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = "BASE TABLE" AND ';
             $sql .= 'TABLE_SCHEMA=:dbName';
@@ -282,8 +136,7 @@ class Database
      *
      * @return array
      */
-    public function scanTables()
-    {
+    public function scanTables() {
         if ($this->connect() === true) {
             $sql = 'SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = "BASE TABLE" AND ';
             $sql .= 'TABLE_SCHEMA=:dbName';
